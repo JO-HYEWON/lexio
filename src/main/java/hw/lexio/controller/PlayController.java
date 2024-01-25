99a9a5bf0c43ea6ddac4a2fa0ee3f470e7b4794d
JO-HYEWON <107868634+JO-HYEWON@users.noreply.github.com>
2024-01-25 22:42:14 +0900

    2024-01-25
    / jpa 세팅 및 postgresql 연결

--- src/main/java/hw/lexio/controller/PlayController.java
+++ src/main/java/hw/lexio/controller/PlayController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/lexio/play")
+@RequestMapping("/lexio")
 @RequiredArgsConstructor
 public class PlayController {
 
